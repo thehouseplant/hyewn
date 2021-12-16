d88cf9d47ead416d00d435fe3dee6499ea320e10
thehouseplant <sean.collins@outlook.com>
2021-12-15 17:12:20 -0800

    Add a simple footer

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,15 @@ impl Component for Model {
                 <main class="container">
                     <Switch<Route> render={Switch::render(switch)} />
                 </main>
+
+                <footer class="container">
+                    <small>
+                        { "Built with " }<a href="https://www.rust-lang.org/">{ "Rust" }</a> 
+                        { " and " }<a href="https://yew.rs/">{ "Yew" }</a>
+                        { " • Styles by "}<a href="https://picocss.com/">{ "Pico" }</a>
+                        { " • " }<a href="https://github.com/thehouseplant/hewn">{ "Source" }</a>
+                    </small>
+                </footer>
             </BrowserRouter>
         }
     }
