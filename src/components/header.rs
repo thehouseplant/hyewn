85032a30a306426aaa1b1aa9719a082904efa7d5
thehouseplant <sean.collins@outlook.com>
2021-12-18 10:46:16 -0800

    Convert to function components

--- src/components/header.rs
+++ src/components/header.rs
@@ -3,52 +3,42 @@ use yew_router::prelude::*;
 
 use crate::Route;
 
-pub struct Header;
-
-impl Component for Header {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <nav class="container-fluid">
-                <ul>
-                    <li>
-                        <strong>{ "HEWN" }</strong>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <Link<Route> to={Route::Top}>
-                            { "top" }
-                        </Link<Route>>
-                    </li>
-                    <li>
-                        <Link<Route> to={Route::New}>
-                            { "new" }
-                        </Link<Route>>
-                    </li>
-                    <li>
-                        <Link<Route> to={Route::Show}>
-                            { "show" }
-                        </Link<Route>>
-                    </li>
-                    <li>
-                        <Link<Route> to={Route::Ask}>
-                            { "ask" }
-                        </Link<Route>>
-                    </li>
-                    <li>
-                        <Link<Route> to={Route::Jobs}>
-                            { "jobs" }
-                        </Link<Route>>
-                    </li>
-                </ul>
-            </nav>
-        }
+#[function_component(Header)]
+pub fn header() -> Html {
+    html! {
+        <nav class="container-fluid">
+            <ul>
+                <li>
+                    <strong>{ "HEWN" }</strong>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link<Route> to={Route::Top}>
+                        { "top" }
+                    </Link<Route>>
+                </li>
+                <li>
+                    <Link<Route> to={Route::New}>
+                        { "new" }
+                    </Link<Route>>
+                </li>
+                <li>
+                    <Link<Route> to={Route::Show}>
+                        { "show" }
+                    </Link<Route>>
+                </li>
+                <li>
+                    <Link<Route> to={Route::Ask}>
+                        { "ask" }
+                    </Link<Route>>
+                </li>
+                <li>
+                    <Link<Route> to={Route::Jobs}>
+                        { "jobs" }
+                    </Link<Route>>
+                </li>
+            </ul>
+        </nav>
     }
 }
